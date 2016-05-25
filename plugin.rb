e8b2307acdeb960c29da17860ed6ccc45143be01
Sam <sam.saffron@gmail.com>
2016-05-25 17:46:06 +1000

    Add setting for authorize options

--- plugin.rb
+++ plugin.rb
@@ -20,6 +20,8 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
                           authorize_url: SiteSetting.oauth2_authorize_url,
                           token_url: SiteSetting.oauth2_token_url
                         }
+                        opts[:authorize_options] = SiteSetting.oauth2_authorize_options.split("|").map(&:to_sym)
+
                         if SiteSetting.oauth2_send_auth_header?
                           opts[:token_params] = {headers: {'Authorization' => basic_auth_header }}
                         end
