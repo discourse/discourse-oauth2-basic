8fc2429496135a53b85a19a35f1a443b4d972614
Leo McArdle <leomcardle@gmail.com>
2017-09-05 20:12:45 +0100

    FIX: make work with user_emails migration

--- plugin.rb
+++ plugin.rb
@@ -104,7 +104,7 @@ class OAuth2BasicAuthenticator < ::Auth::OAuth2Authenticator
     if current_info
       result.user = User.where(id: current_info[:user_id]).first
     elsif SiteSetting.oauth2_email_verified?
-      result.user = User.where(email: Email.downcase(result.email)).first
+      result.user = User.find_by_email(result.email)
       if result.user && user_details[:user_id]
         ::PluginStore.set("oauth2_basic", "oauth2_basic_user_#{user_details[:user_id]}", user_id: result.user.id)
       end
