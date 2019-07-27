ba362493bd5c8e637b7cdf2f631b38ee9564ee8d
Angus McLeod <angus@mcleod.org.au>
2019-07-27 12:26:54 +1000

    Convert migration to pure sql

--- db/migrate/20190724055909_move_to_managed_authenticator.rb
+++ db/migrate/20190724055909_move_to_managed_authenticator.rb
@@ -1,24 +1,20 @@
 class MoveToManagedAuthenticator < ActiveRecord::Migration[5.2]
-  def change
-    ::PluginStoreRow.where(plugin_name: 'oauth2_basic').each do |record|
-      provider_uid = record.key.split('_').last
-      
-      begin
-        value_hash = JSON.parse(record.value)
-        user_id = value_hash["user_id"]
-      rescue JSON::ParserError
-        nil
-      end
-      
-      if provider_uid && user_id
-        UserAssociatedAccount.create(
-          provider_name: 'oauth2_basic',
-          provider_uid: provider_uid,
-          user_id: user_id
-        )
-      end
-      
-      record.destroy
-    end
+  def up
+    execute <<~SQL
+    INSERT INTO user_associated_accounts (
+      provider_name,
+      provider_uid,
+      user_id,
+      created_at,
+      updated_at
+    ) SELECT
+      'oauth2_basic',
+      replace(key, 'oauth2_basic_user_', ''),
+      (value::json->>'user_id')::integer,
+      CURRENT_TIMESTAMP,
+      CURRENT_TIMESTAMP
+    FROM plugin_store_rows
+    WHERE plugin_name = 'oauth2_basic'
+    SQL
   end
 end
