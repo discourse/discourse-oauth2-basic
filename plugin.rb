79696adffc515c3fcb1e5fd79f917ea145ad2ccc
Kalach, Dmitry <d.kalach@itransition.com>
2021-04-19 16:27:49 +0300

    Ignore CSRF check as a plugin setting.

--- plugin.rb
+++ plugin.rb
@@ -91,7 +91,7 @@ class ::OAuth2BasicAuthenticator < Auth::ManagedAuthenticator
                         opts = env['omniauth.strategy'].options
                         opts[:client_id] = SiteSetting.oauth2_client_id
                         opts[:client_secret] = SiteSetting.oauth2_client_secret
-                        opts[:provider_ignores_state] = true
+                        opts[:provider_ignores_state] = SiteSetting.oauth2_disable_csrf
                         opts[:client_options] = {
                           authorize_url: SiteSetting.oauth2_authorize_url,
                           token_url: SiteSetting.oauth2_token_url,
